45a79c3a5616bb288b560249d5882da9f00fa0c8
ABDO-OS <abduos369@gmail.com>
2025-07-20 14:38:28 +0300

    Add new SVG assets and update constants for image paths
    - Added bag.svg for bag icon representation.
    - Introduced dark_theme.svg and light_theme.svg for theme icons.
    - Updated constants.dart to include new image asset paths for easy access.
    - Created a new subscriptionscreen.dart file for future subscription feature implementation.

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -2,3 +2,11 @@ class K {
   static const String fontDin = 'Din';
   static const String isLoggedIn = 'isLoggedIn';
 }
+
+class ImageAssets {
+  static const String appIconDark = 'assets/images/app_icon_dark.svg';
+  static const String appIconLight = 'assets/images/app_icon_white.svg';
+  static const String bag = 'assets/images/bag.svg';
+  static const String iconDarkTheme = 'assets/images/dark_theme.svg';
+  static const String iconLightTheme = 'assets/images/light_theme.svg';
+}
